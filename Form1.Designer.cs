b59921b3cace25058d1adfaa698da9da7eb11a71
afonsosousah <afonsosousah@hotmail.com>
2023-01-05 14:36:32 +0000

    first release

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace xopp_Merger
+{
+    partial class xoppFileMerger
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 431);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose Files to Merge";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(29, 477);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(431, 29);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(29, 55);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(431, 370);
+            this.treeView1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Files to merge:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(276, 431);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Merge Files";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // xoppFileMerger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 522);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "xoppFileMerger";
+            this.Text = "xopp File Merger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace xopp_Merger
+{
+    public partial class xoppFileMerger : Form
+    {
+        string[] fileNamesToMerge;
+        string mergedFileName;
+
+        public xoppFileMerger()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Xournal++ file (*.xopp)|*.xopp";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileNamesToMerge = openFileDialog.FileNames;
+                foreach (string fileName in fileNamesToMerge)
+                {
+                    treeView1.Nodes.Add(new TreeNode(fileName));
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string tempPath = Path.GetTempPath();
+            DirectoryInfo tempFolder = Directory.CreateDirectory(tempPath + "/xoppFileMerger/"); // create temp folder
+
+            string mergedXmlString = "";
+
+            progressBar1.Maximum = fileNamesToMerge.Length + 1; // Set number of steps as max value of progress
+
+            for (int i = 0; i < fileNamesToMerge.Length; i++)
+            {
+                string fileName = fileNamesToMerge[i];
+
+                // Store a copy of the files in the temp folder
+                string tempFileName = tempFolder.FullName + Path.GetFileName(fileName);
+                File.Copy(fileName, tempFileName, true); // overwrite if it exists
+
+                // Decompress the files to xml
+                using (FileStream compressedStream = File.Open(tempFileName, FileMode.Open))
+                using (var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+                using (var resultStream = new MemoryStream())
+                {
+                    zipStream.CopyTo(resultStream);
+                    if (i == 0)
+                    {
+                        using (StreamReader streamReader = new StreamReader(resultStream))
+                        {
+                            // Read xml file as a string
+                            resultStream.Seek(0, SeekOrigin.Begin);
+                            string xmlString = streamReader.ReadToEnd();
+
+                            // Remove the xournal ending tag
+                            xmlString = xmlString.Replace("</xournal>","");
+
+                            // Append to the output string
+                            mergedXmlString += xmlString;
+                        }
+                    }
+                    else
+                    {
+                        using (StreamReader streamReader = new StreamReader(resultStream))
+                        {
+                            // Read xml file as a string
+                            resultStream.Seek(0, SeekOrigin.Begin);
+                            string xmlString = streamReader.ReadToEnd();
+
+                            // Remove first 4 lines and ending tag
+                            var lines = Regex.Split(xmlString, "\r\n|\r|\n").Skip(4);
+                            xmlString = string.Join(Environment.NewLine, lines.ToArray());
+                            xmlString = xmlString.Replace("</xournal>", "");
+
+                            // Append to the output string
+                            mergedXmlString += xmlString;
+                        }
+                    }
+                }
+
+                // Update progress
+                progressBar1.Value = i;
+            }
+
+            // Add the ending tag to the string and remove empty lines
+            mergedXmlString += "\n</xournal>";
+            mergedXmlString = Regex.Replace(mergedXmlString, @"^\s+$[\r\n]*", string.Empty, RegexOptions.Multiline);
+
+            // Save string and compress to xopp file
+            using (var compressedStream = new FileStream(tempFolder + "merged.xopp", FileMode.Create))
+            using (var zipStream = new GZipStream(compressedStream, CompressionMode.Compress))
+            {
+                var data = Encoding.ASCII.GetBytes(mergedXmlString);
+                zipStream.Write(data, 0, data.Length);
+            }
+            mergedFileName = tempFolder + "merged.xopp";
+
+            // Process complete
+            progressBar1.Value = progressBar1.Maximum;
+            MessageBox.Show("Merge completed!");
+
+            // Save the merged file
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Xournal++ file (*.xopp)|*.xopp";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (!string.IsNullOrEmpty(mergedFileName))
+                {
+                    File.Copy(mergedFileName, saveFileDialog.FileName, true); // overwrite if user chooses to
+                }
+                else
+                {
+                    MessageBox.Show("No merged file found. Try merging again.");
+                }
+            }
+
+            // Cleanup
+            foreach(FileInfo file in tempFolder.EnumerateFiles())
+            {
+                file.Delete();
+            }
+            tempFolder.Delete();
+        }
+    }
+}
